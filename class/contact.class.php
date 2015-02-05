db84399569d607022b401fc7c7254741fb242b45
Damien Senger <mail@damiensenger.me>
2015-02-05 17:09:40 +0100

    Contact module init

---
+++ class/contact.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package     LeQG
+ * @author      Damien Senger <tech@leqg.info>
+ * @copyright   2014-2015 MSQG SAS – LeQG
+ */
+
+class Contact
+{
+    /**
+     * Contact class init method
+     * 
+     * This method can call every asked submodule of contact and launch every API request
+     *
+     * @version 1.0
+     * @return  void
+     */
+    
+    public function __construct()
+    {
+        
+    }
+}
